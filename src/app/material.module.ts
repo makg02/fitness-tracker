3951291188e91e0c30318f3000a0563799224fad
Mack <gayonmarkanthony@gmail.com>
2019-06-21 17:09:55 -0800

    Adding angular material to a project and flexbbox

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule
+} from "@angular/material";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
 export class MaterialModule {}
